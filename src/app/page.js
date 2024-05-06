724db6bb1269702716f2a9bcd4d30bea905208c6
HHok95 <S3936933@student.rmit.edu.au>
2024-05-06 22:17:54 +1000

    Add MenuOverlay and NavBar components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import React from "react";
 import HeroSection from "./Components/HeroSection";
+import NavBar from "./Components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div class="container mx-auto px-12 py-4">
+      <NavBar />
+      <div class="container mx-auto px-12 py-4 mt-24">
         <HeroSection />
       </div>
     </main>
