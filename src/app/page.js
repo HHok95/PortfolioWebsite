3d87e68c3420dc9272c6d4ae4bfdac66a52cd019
HHok95 <S3936933@student.rmit.edu.au>
2024-05-10 21:45:39 +1000

    Design Experience Card

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import NavBar from "./Components/NavBar";
 import FooterSection from "./Components/FooterSection";
 import AboutSection from "./Components/AboutSection";
 import ProjectsSection from "./Components/ProjectsSection";
-
+import ExperienceSection from "./Components/ExperienceSection";
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
       <div className="container mx-auto px-12 py-4 mt-0">
         <HeroSection />
         <AboutSection />
+        <ExperienceSection />
         <ProjectsSection />
       </div>
       <FooterSection />
